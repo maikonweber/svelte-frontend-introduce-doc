2e710507dfc940c591541fbae07b7fa793fcd7a4
Maikon <m.carvalho@grouplinknetwork.com>
2023-07-02 13:42:05 -0300

    Add button and colors and select

--- src/routes/Nested.svelte
+++ src/routes/Nested.svelte
@@ -8,8 +8,9 @@
 		website: 'https://svelte.dev'
 	};
 
+    const colors = ['red', 'orange', 'yellow'];
 
-
+    let selected = colors[0];
 
     let numbers = [1, 2, 3, 4];
     let count = 0;
@@ -64,15 +65,72 @@
 <p>{count} - doubles id {doubled}</p>
 
 
+
+{#if count > 10} 
+    <p>
+        {count} Count is grather than 10</p>
+{:else if count < 5}
+        <p>{count} is less than 5</p>
+{:else}
+<p> {count} is beetween 0 and 10 </p>
+
+{/if}
+
+
+<h1 style="color: {selected}">
+    Pick a colour 
+</h1>
+
+<div>
+    <button
+        aria-current={selected === 'red'}
+        aria-label="red"
+        style="background: red"
+        on:click={() => selected = 'red'}
+    >
+    </button>
+    <button
+        aria-current={selected === 'orange'}
+        aria-label="orange"
+        style="background: orange"
+        on:click={() => selected = 'orange'}
+    >
+    </button>
+    <button
+        aria-current={selected === 'yellow'}
+        aria-label="yellow"
+        style="background: yellow"
+        on:click={() => selected = 'yellow'}
+    >
+    </button>
+</div>
+
 <style>
-    button {
-        background-color: blue;
-        font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-        width: 200px;
-        height: 35px;
-        font-size: 20px;
-        border-color: blueviolet;
-        border-style: double;
-        border-radius: 5px;       
-    }
+    	h1 {
+		transition: color 0.2s;
+	}
+
+	div {
+		display: grid;
+		grid-template-columns: repeat(7, 1fr);
+		grid-gap: 5px;
+		max-width: 400px;
+	}
+
+	button {
+		aspect-ratio: 1;
+		border-radius: 50%;
+		background: var(--color, #fff);
+		transform: translate(-2px,-2px);
+		filter: drop-shadow(2px 2px 3px rgba(0,0,0,0.2));
+		transition: all 0.1s;
+	}
+
+	button[aria-current="true"] {
+		transform: none;
+		filter: none;
+		box-shadow: inset 3px 3px 4px rgba(0,0,0,0.2);
+	}
 </style>
+
+
