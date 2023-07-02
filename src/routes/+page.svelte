c12409fa9e79169097fbc1c50cfdd3a67ba5849c
Maikon <m.carvalho@grouplinknetwork.com>
2023-07-02 00:10:17 -0300

    styling

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    let src = '../../static/favicon.png';
+    let name = "MutterCorp";
+
+</script>
+
+
+<h1>  Welcome !!! {name.toUpperCase()} ! </h1>
+
+<img src={src} alt='A favicon '/>
+
+
+<style>
+    h1 {
+        color : goldenrod;
+        
+    }
+
+</style>
