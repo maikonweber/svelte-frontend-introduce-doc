9db43b5c8ef403c31f11b5b5fe4ccb14329512ad
Maikon <m.carvalho@grouplinknetwork.com>
2023-07-02 14:02:17 -0300

    Add each script and slice array of itens

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,22 @@
 <script>
 // @ts-nocheck
-
-    let name = 'MutterCorp'
     import Nested from "./Nested.svelte";
     import Packageinfo from "./Packageinfo.svelte";
+    import Thing from './Thing.svelte';
+    
+    let name = 'MutterCorp'
+    
+    let things = [
+        { id: 1, name: 'apple'},
+        { id: 1, name: 'banana'},
+        { id: 1, name: 'carrot'},
+        { id: 1, name: 'egg'},
+    ];
+
+    function handleClick() {
+        things = things.slice(1);
+    }
+
 
     const pkg = {
         name : 'svelte',
@@ -13,6 +26,14 @@
     }
 </script>
 
+
+{#each things as thing}
+    <Thing name={thing.name} />
+{/each}
+
+<button on:click={handleClick}>
+    Remove first thing
+</button>
 
 <h1>  Welcome !!! {name.toUpperCase()} ! </h1>
 
@@ -25,11 +46,19 @@
 </Packageinfo>
 
 
+
 <style>
     h1 {
         color : goldenrod;
         font-family:'Comic Sans MS', cursive;
         font-size: 2em;
+    }
+
+    button {
+        background-color: blueviolet;
+        color: brown;
+        width:'50px';
+        height:'50px';
     }
 
 </style>
