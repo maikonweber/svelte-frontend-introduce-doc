3370258e1c633fb1d1240069b3ea484062671614
Maikon <m.carvalho@grouplinknetwork.com>
2023-07-02 14:55:18 -0300

    aswer blind input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,53 @@
 <script>
-	let a = 1;
-	let b = 2;
+	let questions = [
+		{
+			id: 1,
+			text: `Where did you go to school?`
+		},
+		{
+			id: 2,
+			text: `What is your mother's name?`
+		},
+		{
+			id: 3,
+			text: `What is another personal fact that an attacker could easily find with Google?`
+		}
+	];
+
+	let selected;
+
+	let answer = '';
+
+	function handleSubmit() {
+		alert(
+			`answered question ${selected.id} (${selected.text}) with "${answer}"`
+		);
+	}
 </script>
 
-<label>
-	<input type="number" bind:value={a} min="0" max="10" />
-	<input type="range" bind:value={a} min="0" max="10" />
-</label>
+<h2>Insecurity questions</h2>
 
-<label>
-	<input type="number" bind:value={b} min="0" max="10" />
-	<input type="range" bind:value={b} min="0" max="10" />
-</label>
+<form on:submit|preventDefault={handleSubmit}>
+	<select
+		bind:value={selected}
+		on:change={() => (answer = '')}
+	>
+		{#each questions as question}
+			<option value={question}>
+				{question.text}
+			</option>
+		{/each}
+	</select>
 
-<script>
-	let yes = false;
-</script>
+	<input bind:value={answer} />
 
-<label>
-	<input type="checkbox" bind:checked={yes} />
-	Yes! Send me regular email spam
-</label>
+	<button disabled={!answer} type="submit">
+		Submit
+	</button>
+</form>
 
-{#if yes}
-	<p>
-		Thank you. We will bombard your inbox and sell
-		your personal details.
-	</p>
-{:else}
-	<p>
-		You must opt in to continue. If you're not
-		paying, you're the product.
-	</p>
-{/if}
+<p>
+	selected question {selected
+		? selected.id
+		: '[waiting...]'}
+</p>
