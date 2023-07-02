a43bc42acf5910ffbe0f44673a5b65043e4e2592
Maikon <m.carvalho@grouplinknetwork.com>
2023-07-02 16:48:16 -0300

    Add store timer annd ppage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 
 <script>
-    import Chat from "../components/chat/chat.component.svelte";
+import Chat from "../components/chat/chat.component.svelte";
 import Gradient from "../components/gradient/gradient.svelte";
 import Header from "../components/header/header.svelte";
+import Store from "../components/store/store.svelte";
 </script>
 
-<Header></Header>
-<Gradient></Gradient>
+<!-- <Header></Header> -->
+<!-- <Gradient></Gradient> -->
 <!-- <Chat></Chat> -->
+<Store></Store>
