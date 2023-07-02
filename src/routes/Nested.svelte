7e4a530e7be8fee1f1d2d136f280952ab8c088d9
Maikon <m.carvalho@grouplinknetwork.com>
2023-07-02 00:19:33 -0300

    Declarations and Button Reactivy

--- src/routes/Nested.svelte
+++ src/routes/Nested.svelte
@@ -1,1 +1,26 @@
+<script>
+    let count = 0;
+
+
+    function increment () {
+        count += 1;
+    }
+$: doubled = count * 2;
+
+</script>
+
+
+
+
+
+
 <h1> This another component </h1>
+
+<button on:click={increment}> 
+    Clicked {count}
+   
+    {count === 1 ?  'time' : 'times'}
+</button>
+
+<p>{count} - doubles id { doubled }</p>
+
